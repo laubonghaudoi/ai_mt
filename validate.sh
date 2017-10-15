b20eabd475e78bb2aeb5bd610d53d0057d104adf
laubonghaudoi <liubanghoudai24@gmail.com>
2017-10-15 23:55:26 +0800

    Update BLEU

--- validate.sh
+++ validate.sh
@@ -9,5 +9,7 @@
 # 将valid.en-zh.zh.sgm分割为valid.en-zh.zh.seg.sgm
 ./tools/chi_char_segment.pl -t xml < outputs/ex1/valid.en-zh.zh.sgm > outputs/ex1/valid.en-zh.zh.seg.sgm
 
-# 计算BLEU
-./tools/mteval-v11b.pl -s outputs/ex1/valid.en-zh.en.sgm -r outputs/ex1/valid.en-zh.zh.seg.sgm -t outputs/ex1/out1.seg.sgm -c > score/out1.bleu
+# 计算BLEU，输出至score/out1.bleu
+./tools/mteval-v11b.pl -s outputs/ex1/valid.en-zh.en.sgm -r outputs/ex1/valid.en-zh.zh.seg.sgm -t outputs/ex1/out1.seg.sgm -c > score/ex1.bleu
+# 计算BLEU并打印
+python ./tools/mt-score-main.py -rs outputs/ex1/valid.en-zh.zh.sgm -hs outputs/ex1/out1.sgm -ss outputs/ex1/valid.en-zh.en.sgm --id ex1 | tee score/ex1.score
