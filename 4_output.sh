e0de2c88ac43fd785adc85193eca65757515000c
laubonghaudoi <liubanghoudai24@gmail.com>
2017-10-25 11:28:25 +0800

    Fix

--- 4_output.sh
+++ 4_output.sh
@@ -2,7 +2,7 @@
 
 # Step 3:
 # Output inferences.
-python translate.py \
+python nmt/translate.py \
 -model ckpts/demo-model_acc_51.94_ppl_12.01_e4.pt \
 -src data/valid.en \
 -output outputs/exp.txt \
