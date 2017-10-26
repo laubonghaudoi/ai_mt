97d43104ce61d9cafc24cc07083e70c8fabe08d4
laubonghaudoi <liubanghoudai24@gmail.com>
2017-10-27 02:33:26 +0800

    Update wrap

--- wrap.sh
+++ wrap.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-./tools/wrap_xml.pl zh ./raw_data/translation_validation_20170912/valid.en-zh.en.sgm exp < outputs/exp.txt > outputs/exp.sgm
+./tools/wrap_xml.pl zh ./raw_data/translation_validation_20170912/valid.en-zh.en.sgm exp < outputs/exp.txt > uploads/exp.sgm
