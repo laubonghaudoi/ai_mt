502bdff561d63a91d45439cbd34563ce271477b0
laubonghaudoi <liubanghoudai24@gmail.com>
2017-10-26 09:16:42 +0800

    Fix train

--- 2_preprocess.sh
+++ 2_preprocess.sh
@@ -5,8 +5,8 @@
 # Read source and target data and save as OpenNMT torchtext objects
 # Train file is save as `data/exp.train.pt`, validation file `data/exp.valid.pt``
 python nmt/preprocess.py \
--train_src tokenized_jieba_data/train.en \
--train_tgt tokenized_jieba_data/train.zh \
+-train_src tokenized_jieba_data/train_2.en \
+-train_tgt tokenized_jieba_data/train_2.zh \
 -valid_src tokenized_jieba_data/valid.en \
 -valid_tgt tokenized_jieba_data/valid.zh \
--save_data data/exp
+-save_data dataset/cross_2
--- 3_nmt.sh
+++ 3_nmt.sh
@@ -2,4 +2,9 @@
 
 # Step 2:
 # Start training. Models are saved as `exp-model_acc_X_ppl_X_eX.pt`
-python nmt/train.py -data data/demo -save_model exp-model -train_from ckpts/demo-model_acc_51.94_ppl_12.01_e4.pt -gpuid 0 -start_epoch 5 -batch_size 64 -epochs 10 -optim adam -learning_rate 0.001
+python nmt/train.py \
+-data dataset/cross_2 \
+-save_model ckpts/exp-model \
+-train_from ckpts/demo-model_acc_51.94_ppl_12.01_e4.pt \
+-start_epoch 5 \
+-gpuid 0
